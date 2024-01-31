5499f6496f88ac17bb3e35fe4ead714bf03888a4
David <paulussen86@gmail.com>
2024-01-31 09:12:04 +0100

    update

---
+++ com/cc/java/HoneyBee.java
@@ -0,0 +1,13 @@
+package com.cc.java;
+
+public class  HoneyBee {
+
+public String doYourJob() {
+    return "I´m a HoneyBee";
+}
+
+public String fly() {
+    return "I belive I can fly";
+}
+    
+}
